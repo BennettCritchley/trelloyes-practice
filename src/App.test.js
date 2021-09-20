eb5b6019d03a6ef165fb903841b5b8c0c8540e19
Bennett Critchley <bennett.critchley@hotmail.com>
2021-09-20 14:31:20 -0600

    added smoke test

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import card from './card/card'
+import list from './list/list'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('should render the DOM and a card', () => {
+  expect(card()).toBe(true);
+});
+
+it('it should render multiple cards', () => {
+  expect(list.length()).toBe(true);
+})
